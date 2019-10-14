e423dd9f921bc07ab0690404d1b7b5f59d074d20
jvrdms <jvrdms@gmail.com>
2019-10-14 19:46:31 +0200

    Paging

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -20,11 +20,20 @@ namespace ContosoUniversity.Pages.Students {
     public string CurrentFilter { get; set; }
     public string CurrentSort { get; set; }
 
-    public IList<Student> Student { get; set; }
+    public PaginatedList<Student> Student { get; set; }
 
-    public async Task OnGetAsync(string sortOrder, string searchString) {
+    public async Task OnGetAsync(string sortOrder, string currentFilter,
+                                 string searchString, int? pageIndex) {
+      CurrentSort = sortOrder;
       NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
       DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+      if (searchString != null) {
+        pageIndex = 1;
+      }
+      else {
+        searchString = currentFilter;
+      }
       CurrentFilter = searchString;
 
       IQueryable<Student> studentIQ = from s in _context.Student
@@ -50,7 +59,10 @@ namespace ContosoUniversity.Pages.Students {
           break;
       }
 
-      Student = await studentIQ.AsNoTracking().ToListAsync();
+      int pageSize = 3;
+      Student = await PaginatedList<Student>.CreateAsync(
+        studentIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+      //Student = await studentIQ.AsNoTracking().ToListAsync();
     }
   }
 }
