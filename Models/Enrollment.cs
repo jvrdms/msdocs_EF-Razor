c20f9c8d694296281c40ad753be2f41c841a4d0f
jvrdms <jvrdms@gmail.com>
2019-10-17 22:08:19 +0200

    Create a complex data model (Before migration)

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoUniversity.Models {
   public enum Grade {
     A, B, C, D, F
@@ -7,6 +9,8 @@ namespace ContosoUniversity.Models {
     public int EnrollmentID { get; set; }
     public int CourseID { get; set; }
     public int StudentID { get; set; }
+
+    [DisplayFormat(NullDisplayText = "No grade")]
     public Grade? Grade { get; set; }
 
     public Course Course { get; set; }
