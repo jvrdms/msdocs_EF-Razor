49ddc8c8d706706c08316e8acfe01bc2507b472d
jvrdms <jvrdms@gmail.com>
2019-10-19 17:00:22 +0200

    Complex data model (Database dropped)

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -15,23 +15,23 @@ namespace ContosoUniversity.Data {
       }
 
       var students = new Student[] {
-        new Student { FirstMidName = "Carson",   LastName = "Alexander",
-            EnrollmentDate = DateTime.Parse("2010-09-01") },
-        new Student { FirstMidName = "Meredith", LastName = "Alonso",
-            EnrollmentDate = DateTime.Parse("2012-09-01") },
-        new Student { FirstMidName = "Arturo",   LastName = "Anand",
-            EnrollmentDate = DateTime.Parse("2013-09-01") },
-        new Student { FirstMidName = "Gytis",    LastName = "Barzdukas",
-            EnrollmentDate = DateTime.Parse("2012-09-01") },
-        new Student { FirstMidName = "Yan",      LastName = "Li",
-            EnrollmentDate = DateTime.Parse("2012-09-01") },
-        new Student { FirstMidName = "Peggy",    LastName = "Justice",
-            EnrollmentDate = DateTime.Parse("2011-09-01") },
-        new Student { FirstMidName = "Laura",    LastName = "Norman",
-            EnrollmentDate = DateTime.Parse("2013-09-01") },
-        new Student { FirstMidName = "Nino",     LastName = "Olivetto",
-            EnrollmentDate = DateTime.Parse("2005-09-01") }
-      };
+            new Student { FirstMidName = "Carson",   LastName = "Alexander",
+                EnrollmentDate = DateTime.Parse("2010-09-01") },
+            new Student { FirstMidName = "Meredith", LastName = "Alonso",
+                EnrollmentDate = DateTime.Parse("2012-09-01") },
+            new Student { FirstMidName = "Arturo",   LastName = "Anand",
+                EnrollmentDate = DateTime.Parse("2013-09-01") },
+            new Student { FirstMidName = "Gytis",    LastName = "Barzdukas",
+                EnrollmentDate = DateTime.Parse("2012-09-01") },
+            new Student { FirstMidName = "Yan",      LastName = "Li",
+                EnrollmentDate = DateTime.Parse("2012-09-01") },
+            new Student { FirstMidName = "Peggy",    LastName = "Justice",
+                EnrollmentDate = DateTime.Parse("2011-09-01") },
+            new Student { FirstMidName = "Laura",    LastName = "Norman",
+                EnrollmentDate = DateTime.Parse("2013-09-01") },
+            new Student { FirstMidName = "Nino",     LastName = "Olivetto",
+                EnrollmentDate = DateTime.Parse("2005-09-01") }
+        };
 
       foreach (Student s in students) {
         context.Student.Add(s);
